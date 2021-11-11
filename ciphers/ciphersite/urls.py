70c20994310b9bb76403a4ad42cc94a36b451762
Christian Tingle <ctingle18@gmail.com>
2021-11-10 23:51:01 -0500

    initial site

--- ciphers/ciphersite/urls.py
+++ ciphers/ciphersite/urls.py
@@ -3,4 +3,8 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('vigenere/', views.vigenere, name='vigenere'),
+    path('des/', views.des, name='des'),
+    path('rsa/', views.rsa, name='rsa'),
+    path('md5/', views.md5, name='md5'),
 ]
--- ciphers/ciphersite/views.py
+++ ciphers/ciphersite/views.py
@@ -1,5 +1,28 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.template import loader
+
 
 def index(request):
-    return HttpResponse("Hello, world")
+    context = {}
+    return render(request, 'ciphersite/index.html', context)
+
+
+def vigenere(request):
+    context = {}
+    return render(request, 'ciphersite/vigenere.html', context)
+
+
+def des(request):
+    context = {}
+    return render(request, 'ciphersite/des.html', context)
+
+
+def rsa(request):
+    context = {}
+    return render(request, 'ciphersite/rsa.html', context)
+
+
+def md5(request):
+    context = {}
+    return render(request, 'ciphersite/md5.html', context)
