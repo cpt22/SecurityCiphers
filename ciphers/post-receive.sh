ba70e09b5ea6d0ee081cede985fddb07424d31c0
Christian Tingle <ctingle18@gmail.com>
2021-11-12 15:23:33 -0500

    Added postreceive hook

---
+++ ciphers/post-receive.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+cd /home/deploy/SecurityCiphers
+git checkout master && git pull -f
+source /home/deploy/SecurityCiphers/venv/bin/activate
+python /home/deploy/SecurityCiphers/ciphers/manage.py makemigrations
+python /home/deploy/SecurityCiphers/ciphers/manage.py migrate
+python /home/deploy/SecurityCiphers/ciphers/manage.py collectstatic --noinput
+sudo /bin/systemctl restart ciphersite
