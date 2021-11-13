eab36197254799a9d7a19a4c6f30848b0ea64a19
Christian Tingle <ctingle18@gmail.com>
2021-11-13 12:05:14 -0500

    updated env

--- ciphers/ciphers/settings.py
+++ ciphers/ciphers/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = True if config('ENVIRONMENT') == 'development' else False
 
 ALLOWED_HOSTS = [
     'localhost',
--- ciphers/post-receive.sh
+++ ciphers/post-receive.sh
@@ -1,9 +1,17 @@
 #!/bin/bash
+echo "Pulling from master"
 git -C /home/deploy/SecurityCiphers checkout master -f
 git -C /home/deploy/SecurityCiphers pull origin
+echo "Activating VENV"
 source /home/deploy/SecurityCiphers/venv/bin/activate
+echo "Installing new requirements"
+pip3 install -r /home/deploy/SecurityCiphers/requirements.txt
+echo "Migrating DB"
 python /home/deploy/SecurityCiphers/ciphers/manage.py makemigrations
 python /home/deploy/SecurityCiphers/ciphers/manage.py migrate
+echo "Collecting static files"
 python /home/deploy/SecurityCiphers/ciphers/manage.py collectstatic --noinput
+
+echo "Restarting site uWSGI"
 sudo /bin/systemctl restart ciphersite
 echo "Landed $1 commits on $2 at $(date)" >> /home/deploy/webhook_log.txt
