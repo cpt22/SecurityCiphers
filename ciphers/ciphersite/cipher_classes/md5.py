03e86e8793753bdb59be9f457c435e48006f6559
Christian Tingle <ctingle18@gmail.com>
2021-11-23 16:44:03 -0600

    temporary hold so i dont lose stuff

--- ciphers/ciphersite/cipher_classes/md5.py
+++ ciphers/ciphersite/cipher_classes/md5.py
@@ -5,74 +5,168 @@ import math
 # https://www.educba.com/md5-alogrithm/
 # https://en.wikipedia.org/wiki/MD5#Algorithm
 class MD5:
-    # Buffer values in little endian format
-    initial_abcd = [0x67452301, 0xefcdab89, 0x98badcfe, 0x10325476]
-
-    F = lambda self, x, y, z: (x & y) | (~x & z)
-    G = lambda self, x, y, z: (x & z) | (y & ~z)
-    H = lambda self, x, y, z: x ^ y ^ z
-    I = lambda self, x, y, z: y ^ (x | ~z)
-
     rotation_arr = [7, 12, 17, 22, 7, 12, 17, 22, 7, 12, 17, 22, 7, 12, 17, 22,
                     5,  9, 14, 20, 5,  9, 14, 20, 5,  9, 14, 20, 5,  9, 14, 20,
                     4, 11, 16, 23, 4, 11, 16, 23, 4, 11, 16, 23, 4, 11, 16, 23,
                     6, 10, 15, 21, 6, 10, 15, 21, 6, 10, 15, 21, 6, 10, 15, 21]
 
-    constants = [int(math.floor(2**32 * abs(math.sin(i + 1)))) & 0xFFFFFFFF for i in range(64)]
+    constants = [int(abs(math.sin(i + 1)) * (2 ** 32)) & 0xFFFFFFFF for i in range(len(rotation_arr))]
 
+    @staticmethod
+    def E(x, y, z):
+        return (x & y) | (~x & z)
+
+    @staticmethod
+    def F(x, y, z):
+        return (x & z) | (y & ~z)
+
+    @staticmethod
+    def G(x, y, z):
+        return x ^ y ^ z
+
+    @staticmethod
+    def H(x, y, z):
+        return y ^ (x | ~z)
+
+    initial_counter_values = [
+        0x67452301,
+        0xefcdab89,
+        0x98badcfe,
+        0x10325476,
+    ]
+
+    shuffle_index_functions = [
+        lambda ind: ind,
+        lambda ind: (5 * ind + 1) % 16,
+        lambda ind: (3 * ind + 5) % 16,
+        lambda ind: (7 * ind) % 16,
+    ]
+
+    @staticmethod
     def hash(self, data):
         data = bytearray(data)
-        length = (8 * len(data)) & 0xffffffffffffffff
+        # calculate the length of data in bits and L_AND that with 0xFFFFFFFFFFFFFFFF to restrict the size to 64 bits
+        data_length = (8 * len(data)) & 0xFFFFFFFFFFFFFFFF
+        # Append the first padding bit
         data.append(0x80)
-        while (len(data) * 8) % 64 != 56:
+        # Pad data with zeroes until it is correctly size aligned
+        while len(data) % 64 != 56:
             data.append(0x00)
-        data += length.to_bytes(8, byteorder="little")
+        # Append the data length to the data byte array in little endian order
+        data += data_length.to_bytes(8, byteorder="little")
 
-        a0, b0, c0, d0 = self.initial_abcd[0:4]
+        # duplicate the initial values into a new arr
+        segments = self.initial_counter_values[:]
 
-        # Loop through the data in 512 bit chunks. The step is 64 since 64 bytes *8 bits/byte = 512 bits
+        # Loop through all of the data in 512 bit (64 byte, hence the 64 step) chunks
         for chunk_512 in range(0, len(data), 64):
-            chunk = data[chunk_512:chunk_512 + 64]
-            a, b, c, d = a0, b0, c0, d0
-            
+            a, b, c, d = segments
+            print(segments)
+            # Obtain 512 bits (64 bytes) of data (the current chunk)
+            current_chunk = data[chunk_512:chunk_512+64]
             for i in range(64):
-                print(f'i:{i}, b:{b}, c:{c}, d:{d}')
                 if i in range(16):
-                    print(chunk)
-                    f = self.F(b, c, d)
+                    f = MD5.E(b, c, d)
                     g = i
                 elif i in range(16, 32):
-                    print("b")
-                    f = self.G(b, c, d)
+                    f = MD5.F(b, c, d)
                     g = (5 * i + 1) % 16
                 elif i in range(32, 48):
-                    print("c")
-                    f = self.H(b, c, d)
+                    f = MD5.G(b, c, d)
                     g = (3 * i + 5) % 16
                 elif i in range(48, 64):
-                    print("d")
-                    f = self.I(b, c, d)
+                    f = MD5.H(b, c, d)
                     g = (7 * i) % 16
 
-                rot = a + f + self.constants[i] + int.from_bytes(chunk[4*g:4*g+4], byteorder='little')
-                rot_output = (b + self.left_rotate(rot, self.rotation_arr[i])) & 0xFFFFFFFF
-                a = d
-                b = rot_output
-                c = b
-                d = c
+                # calculates the index to get the correct function from the arrays
+                ind = math.floor(i / 16)
+                # Calls the correct lambda to process the values
+                f = self.bitwise_functions[ind](b, c, d)
+                g = self.shuffle_index_functions[ind](i)
+                # Calculates the value to be rotated
+                pre_rotation_value = a + f + self.constants[i] + int.from_bytes(current_chunk[4*g:4*g+4], byteorder="little")
+                # Gets the correct new b value after rotation, the 0xFFFFFFFF is used to ensure that the ouput is limited to 8 bytes
+                post_rotation_value = (b + self.left_rotate(pre_rotation_value, self.rotation_arr[i])) & 0xFFFFFFFF
+                a, b, c, d = d, post_rotation_value, b, c
 
-            a0 = (a + a0) & 0xFFFFFFFF
-            b0 = (b + b0) & 0xFFFFFFFF
-            c0 = (c + c0) & 0xFFFFFFFF
-            d0 = (d + d0) & 0xFFFFFFFF
+                for ind, val in enumerate([a, b, c, d]):
+                    segments[i] += val
+                    segments[i] &= 0xFFFFFFFF
 
-            items = [a0 << 0, b0 << 32, c0 << 64, d0 << 96]
+            return sum(x << (32 * i) for i, x in enumerate(segments))
 
-        return sum(items) #sum(x << (32*i) for i, x in enumerate(items))
-
-    def left_rotate(self, x, amnt):
+    def left_rotate(self, x, amount):
         x &= 0xFFFFFFFF
-        return ((x << amnt) | (x >> (32 - amnt))) & 0xFFFFFFFF
+        return ((x << amount) | (x >> (32 - amount))) & 0xFFFFFFFF
+
+# rotation_arr = [7, 12, 17, 22, 7, 12, 17, 22, 7, 12, 17, 22, 7, 12, 17, 22,
+#                 5,  9, 14, 20, 5,  9, 14, 20, 5,  9, 14, 20, 5,  9, 14, 20,
+#                 4, 11, 16, 23, 4, 11, 16, 23, 4, 11, 16, 23, 4, 11, 16, 23,
+#                 6, 10, 15, 21, 6, 10, 15, 21, 6, 10, 15, 21, 6, 10, 15, 21]
+#
+# constants = [int(abs(math.sin(i + 1)) * (2 ** 32)) & 0xFFFFFFFF for i in range(len(rotation_arr))]
+#
+# bitwise_functions = [
+#     lambda x, y, z: (x & y) | (~x & z),
+#     lambda x, y, z: (x & z) | (y & ~z),
+#     lambda x, y, z: x ^ y ^ z,
+#     lambda x, y, z: y ^ (x | ~z),
+# ]
+#
+# initial_counter_values = [
+#             0x67452301,
+#             0xefcdab89,
+#             0x98badcfe,
+#             0x10325476,
+#         ]
+#
+# shuffle_index_functions = [
+#     lambda ind: ind,
+#     lambda ind: (5 * ind + 1) % 16,
+#     lambda ind: (3 * ind + 5) % 16,
+#     lambda ind: (7 * ind) % 16,
+# ]
+#
+# def left_rotate(val, amnt):
+#     val &= 0xFFFFFFFF
+#     return ((val << amnt) | (val >> (32 - amnt))) & 0xFFFFFFFF
+#
+# # Hash the incoming bytearray
+# def hash(data):
+#     data = bytearray(data)
+#     # calculate the length of data in bits and L_AND that with 0xFFFFFFFFFFFFFFFF to restrict the size to 64 bits
+#     data_length = (8 * len(data)) & 0xFFFFFFFFFFFFFFFF
+#     # Append the first padding bit
+#     data.append(0x80)
+#     # Pad data with zeroes until it is correctly size aligned
+#     while len(data) % 64 != 56:
+#         data.append(0x00)
+#     # Append the data length to the data byte array in little endian order
+#     data += data_length.to_bytes(8, byteorder="little")
+#
+#     bits_and_pieces = initial_counter_values[:]
+#
+#     # Loop through all of the data in 512 bit (64 byte, hence the 64 step) chunks
+#     for chunk_512 in range(0, len(data), 64):
+#         a, b, c, d = bits_and_pieces
+#         # Obtain 512 bits (64 bytes) of data (the current chunk)
+#         current_chunk = data[chunk_512:chunk_512+64]
+#         for i in range(64):
+#             # calculates the index to get the correct function from the arrays
+#             ind = math.floor(i / 16)
+#             # Calls the correct lambda to process the values
+#             f = bitwise_functions[ind](b, c, d)
+#             g = shuffle_index_functions[ind](i)
+#             # Calculates the value to be rotated
+#             pre_rotation_value = a + f + constants[i] + int.from_bytes(current_chunk[4*g:4*g+4], byteorder="little")
+#             # Gets the correct new b value after rotation, the 0xFFFFFFFF is used to ensure that the ouput is limited to 8 bytes
+#             post_rotation_value = (b + left_rotate(pre_rotation_value, rotation_arr[i])) & 0xFFFFFFFF
+#             a, b, c, d = d, post_rotation_value, b, c
+#         for i, val in enumerate([a, b, c, d]):
+#             bits_and_pieces[i] += val
+#             bits_and_pieces[i] &= 0xFFFFFFFF
+#
+#         return sum(x << (32 * i) for i, x in enumerate(bits_and_pieces))
 
 
 def md5_to_hex(digest):
@@ -80,9 +174,4 @@ def md5_to_hex(digest):
     return '{:032x}'.format(int.from_bytes(raw, byteorder='big'))
 
 if __name__ == "__main__":
-    st = "poop".encode('utf-8')
-    md5 = MD5()
-    #out = md5(st)
-    out = md5.hash(st)
-    print(md5_to_hex(out))
-    #print(out)
+    print(md5_to_hex(MD5().hash("poop".encode())))
