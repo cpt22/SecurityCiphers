473125a676f6b74311c098c0dc0aad575b751c2b
Anika Washburn <ajw228@cwru.edu>
2021-11-23 20:54:21 -0600

    Can take weird characters

--- ciphers/ciphersite/cipher_classes/des.py
+++ ciphers/ciphersite/cipher_classes/des.py
@@ -1,16 +1,11 @@
 class DESCipher:
-    IP_table = [58, 50, 42, 34, 26, 18, 10, 2, 60, 52, 44, 36, 28, 20, 12, 4, 62, 54, 46, 38, 30, 22, 14, 6, 64, 56, 48,
-                40,
-                32, 24, 16, 8, 57, 49, 41, 33, 25, 17, 9, 1, 59, 51, 43, 35, 27, 19, 11, 3, 61, 53, 45, 37, 29, 21, 13,
-                5,
+    IP_table = [58, 50, 42, 34, 26, 18, 10, 2, 60, 52, 44, 36, 28, 20, 12, 4, 62, 54, 46, 38, 30, 22, 14, 6, 64, 56, 48, 40,
+                32, 24, 16, 8, 57, 49, 41, 33, 25, 17, 9, 1, 59, 51, 43, 35, 27, 19, 11, 3, 61, 53, 45, 37, 29, 21, 13, 5,
                 63, 55, 47, 39, 31, 23, 15, 7]
-    FP_table = [40, 8, 48, 16, 56, 24, 64, 32, 39, 7, 47, 15, 55, 23, 63, 31, 38, 6, 46, 14, 54, 22, 62, 30, 37, 5, 45,
-                13,
-                53, 21, 61, 29, 36, 4, 44, 12, 52, 20, 60, 28, 35, 3, 43, 11, 51, 19, 59, 27, 34, 2, 42, 10, 50, 18, 58,
-                26,
+    FP_table = [40, 8, 48, 16, 56, 24, 64, 32, 39, 7, 47, 15, 55, 23, 63, 31, 38, 6, 46, 14, 54, 22, 62, 30, 37, 5, 45, 13,
+                53, 21, 61, 29, 36, 4, 44, 12, 52, 20, 60, 28, 35, 3, 43, 11, 51, 19, 59, 27, 34, 2, 42, 10, 50, 18, 58, 26,
                 33, 1, 41, 9, 49, 17, 57, 25]
-    right_expansion_table = [32, 1, 2, 3, 4, 5, 4, 5, 6, 7, 8, 9, 8, 9, 10, 11, 12, 13, 12, 13, 14, 15, 16, 17, 16, 17,
-                             18,
+    right_expansion_table = [32, 1, 2, 3, 4, 5, 4, 5, 6, 7, 8, 9, 8, 9, 10, 11, 12, 13, 12, 13, 14, 15, 16, 17, 16, 17, 18,
                              19, 20, 21, 20, 21, 22, 23, 24, 25, 24, 25, 26, 27, 28, 29, 28, 29, 30, 31, 32, 1]
     s_box_table = [[[14, 4, 13, 1, 2, 15, 11, 8, 3, 10, 6, 12, 5, 9, 0, 7],
                     [0, 15, 7, 4, 14, 2, 13, 1, 10, 6, 12, 11, 9, 5, 3, 8],
@@ -53,13 +48,10 @@ class DESCipher:
                     [2, 1, 14, 7, 4, 10, 8, 13, 15, 12, 9, 0, 3, 5, 6, 11]]]
     right_perm_table = [16, 7, 20, 21, 29, 12, 28, 17, 1, 15, 23, 26, 5, 18, 31, 10, 2, 8, 24, 14, 32, 27, 3, 9, 19, 13,
                         30, 6, 22, 11, 4, 25]
-    key_table_56 = [57, 49, 41, 33, 25, 17, 9, 1, 58, 50, 42, 34, 26, 18, 10, 2, 59, 51, 43, 35, 27, 19, 11, 3, 60, 52,
-                    44,
-                    36, 63, 55, 47, 39, 31, 23, 15, 7, 62, 54, 46, 38, 30, 22, 14, 6, 61, 53, 45, 37, 29, 21, 13, 5, 28,
-                    20,
+    key_table_56 = [57, 49, 41, 33, 25, 17, 9, 1, 58, 50, 42, 34, 26, 18, 10, 2, 59, 51, 43, 35, 27, 19, 11, 3, 60, 52, 44,
+                    36, 63, 55, 47, 39, 31, 23, 15, 7, 62, 54, 46, 38, 30, 22, 14, 6, 61, 53, 45, 37, 29, 21, 13, 5, 28, 20,
                     12, 4]
-    key_table_48 = [14, 17, 11, 24, 1, 5, 3, 28, 15, 6, 21, 10, 23, 19, 12, 4, 26, 8, 16, 7, 27, 20, 13, 2, 41, 52, 31,
-                    37,
+    key_table_48 = [14, 17, 11, 24, 1, 5, 3, 28, 15, 6, 21, 10, 23, 19, 12, 4, 26, 8, 16, 7, 27, 20, 13, 2, 41, 52, 31, 37,
                     47, 55, 30, 40, 51, 45, 33, 48, 44, 49, 39, 56, 34, 53, 46, 42, 50, 36, 29, 32]
     keys = []
 
@@ -97,7 +89,7 @@ class DESCipher:
             column_str = ''.join(format(str(i)) for i in part[1:5])
             column_int = int(column_str, 2)
             s_box_int = self.s_box_table[s_box_index][row_int][column_int]
-            for i in format(s_box_int, 'b').zfill(4):  # potentially do differently
+            for i in format(s_box_int, 'b').zfill(4):   # potentially do differently
                 arr_32.append(int(i))
             s_box_index += 1
             index += 6
@@ -139,28 +131,23 @@ class DESCipher:
 
         # Step 6: switch right and left
         final_64 = right_32 + left_32
-        final_perm = self.permute(final_64, self.FP_table)
-        hex_val = hex(int(''.join(format(str(i)) for i in final_perm), 2))[2:]
-        return final_perm, hex_val
+        return self.permute(final_64, self.FP_table)
 
     def des_helper(self, message_bit_arr, key_bit_arr, input_range):
         final_bin = []
-        final_hex_val = ""
         index = 64
         while index <= len(message_bit_arr):
-            bin_arr, hex_val = self.des(message_bit_arr[index - 64:index], key_bit_arr, input_range)
+            bin_arr = self.des(message_bit_arr[index - 64:index], key_bit_arr, input_range)
             final_bin += bin_arr
-            final_hex_val += hex_val
             index += 64
         # Add padding if not all bytes were covered
         if index - 64 < len(message_bit_arr):
-            pad_message_bit_arr = message_bit_arr[index - 64:]
+            pad_message_bit_arr = message_bit_arr[index-64:]
             while len(pad_message_bit_arr) != 64:
                 pad_message_bit_arr.append(0)
-            bin_arr, hex_val = self.des(pad_message_bit_arr, key_bit_arr, input_range)
+            bin_arr = self.des(pad_message_bit_arr, key_bit_arr, input_range)
             final_bin += bin_arr
-            final_hex_val += hex_val
-        return self.hex_to_byte_array(final_hex_val)
+        return self.bit_array_to_byte_array(final_bin)
 
     # Inputs are byte arrays
     def encrypt(self, message_byte_arr, key_byte_arr):
@@ -178,7 +165,7 @@ class DESCipher:
     def permute(input_arr, table):
         permutation = []
         for i in range(0, len(table)):
-            permutation.append(input_arr[table[i] - 1])
+            permutation.append(input_arr[table[i]-1])
         return permutation
 
     @staticmethod
@@ -212,3 +199,13 @@ class DESCipher:
     @staticmethod
     def hex_to_byte_array(hex_value):
         return bytearray(bytes.fromhex(hex_value))
+
+    @staticmethod
+    def bit_array_to_byte_array(bit_array):
+        byte_list = []
+        index = 8
+        while index <= len(bit_array):
+            bin_num = int(''.join(str(i) for i in bit_array[index - 8: index]), 2)
+            byte_list.append(bin_num)
+            index += 8
+        return bytearray(byte_list)
